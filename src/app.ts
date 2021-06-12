5533da52a7cfbc3d980a462501dc7f4dec535d1b
f4rh4d <farhadf4rh4di@gmail.com>
2021-06-12 23:58:03 +0430

    Fixed tsconfig and added some types to dev dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 
+const cors = require('cors');
 const router = require('./router');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 // Creating Connection To Mongo Database
 mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+
 // Connecting To Mongodb
 mongoose.connect('mongodb://localhost:27017/ExonDb').catch((error: any) => {
   console.log('Rejected To Connect To Mongo -> ', error);
