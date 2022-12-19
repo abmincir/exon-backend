a07da8cf1cc2952333448f53b008abf9606b3eb0
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-12-19 22:55:36 +0330

    Added KooK DepositStatement and Login services

---
+++ src/services/KooKService.ts
@@ -0,0 +1,129 @@
+import axios from 'axios';
+import moment from 'moment';
+
+const TOKEN_EXP_MARGIN_MIN = 10;
+
+let TOKEN = ""
+let TOKEN_EXP = ""
+
+interface DepositStatementInputI {
+  cif?: string
+  action?: 'DEBIT' | 'CREDIT'
+  englishDescription?: 'PER' | 'EN'
+  description?: string
+  depositNumber?: string
+  fromDate?: string
+  toDate?: string
+  length?: number
+  offset?: number
+  order?: 'ASC' | 'DESC'
+}
+interface DepositStatementResponseI {
+  totalRecord?: number
+  statements?: {
+    agentBranchCode?: string;
+    agentBranchName?: string;
+    balance?: number;
+    branchCode?: string;
+    branchName?: string;
+    date?: string;
+    description?: string;
+    paymentId?: string;
+    referenceNumber?: string;
+    serial?: number;
+    serialNumber?: string;
+    transferAmount?: number
+  }[]
+}
+
+const depositStatement = async ({
+  cif,
+  action,
+  englishDescription,
+  description,
+  depositNumber,
+  fromDate,
+  toDate,
+  length,
+  offset,
+  order,
+}: DepositStatementInputI) => {
+
+  // TODO: these env must be set
+  refreshToken()
+  const channel = process.env.KOOK_CHANNEL
+  const { data, status } = await axios.post<DepositStatementResponseI>(
+    "https://kook.sb24.ir:9035/deposit/statment", {
+    token: TOKEN,
+    channel,
+    cif,
+    action,
+    englishDescription,
+    description,
+    depositNumber,
+    fromDate,
+    toDate,
+    length,
+    offset,
+    order,
+  });
+
+  if (status === 409) {
+    throw data
+  }
+  return data;
+}
+
+interface LoginResponseI {
+  token?: string
+  expiration?: string;
+
+  error?: string;
+  exception?: string;
+  message?: string;
+  httpStatus?: number;
+  fieldErrors?: {
+    code?: string;
+    field?: string;
+    message?: string;
+    rejectedValue?: any;
+  }[]
+}
+
+const refreshToken = async () => {
+  // TODO: these must be changed in env or in local file
+  const username = process.env.KOOK_USERNAME
+  const password = process.env.KOOK_PASSWORD
+  const channel = process.env.KOOK_CHANNEL
+  const secretKey = process.env.KOOK_SECRET_KEY
+
+  if (!TOKEN_EXP) {
+    // check if token has been expired
+    const diff = moment().diff(TOKEN_EXP, 'minutes')
+    if (diff >= TOKEN_EXP_MARGIN_MIN) {
+      // token exp is yet to be expired, so avoid login
+      return;
+    }
+  }
+
+  // update token
+  const { data } = await axios.post<LoginResponseI>(
+    "https://kook.sb24.ir:9000/login", {
+    channel,
+    username,
+    password,
+    secretKey,
+  });
+
+  if (data.error || data.exception) {
+    const { error, fieldErrors, httpStatus, exception, message } = data
+    throw { error, fieldErrors, httpStatus, exception, message }
+  }
+
+  TOKEN = data.token ?? "";
+  TOKEN_EXP = data.expiration ?? "";
+}
+
+module.exports = {
+  deposit: depositStatement,
+}
