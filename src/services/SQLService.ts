1843f5768853334c548441a119e7b477e6e03ff0
Amir Farhadi <farhaadi.amir@gmail.com>
2023-03-02 09:24:54 +0330

    refactor removed useless code

--- src/services/SQLService.ts
+++ src/services/SQLService.ts
@@ -2,20 +2,6 @@ const sql = require('mssql');
 const fs = require('fs');
 const { Database: DB } = require('../models/Model');
 
-exports.MockData = async () => {
-  return new Promise(async (res, rej) => {
-    try {
-      let rawData = fs.readFileSync('dummy-data.json');
-      let bills = JSON.parse(rawData);
-
-      res(bills);
-    } catch (error: any) {
-      console.error(error);
-      rej(error);
-    }
-  });
-};
-
 exports.FetchData = async (input: {
   startDate: string;
   endDate: string;
@@ -24,35 +10,6 @@ exports.FetchData = async (input: {
   dbId: string;
 }) => {
   const { startDate, endDate, startDateMiladi, endDateMiladi, dbId } = input;
-
-  // config for your database
-  // const config = {
-  //   user: 'sa',
-  //   password: 'mis',
-  //   server: 'srv-siniran\\SRV_SINIRAN',
-  //   database: 'XData',
-  //   options: {
-  //     encrypt: true,
-  //     enableArithAbort: true,
-  //     cryptoCredentialsDetails: {
-  //       minVersion: 'TLSv1',
-  //     },
-  //   },
-  // };
-
-  // const tadbirConfig = {
-  //   user: 'TadbirUser',
-  //   password: '$$$%%%',
-  //   server: '192.168.1.17',
-  //   database: 'TEST1400_6_25',
-  //   options: {
-  //     encrypt: true,
-  //     enableArithAbort: true,
-  //     cryptoCredentialsDetails: {
-  //       minVersion: 'TLSv1',
-  //     },
-  //   },
-  // };
 
   // connect to your database
   return new Promise(async (res, rej) => {
@@ -84,9 +41,6 @@ exports.FetchData = async (input: {
         },
       },
     };
-    console.log('--------- config ------------');
-    console.log(config);
-    console.log('--------- config ------------');
 
     sql.connect(config, (error: Error) => {
       if (error) {
@@ -98,12 +52,12 @@ exports.FetchData = async (input: {
       request.input(
         isShamsi ? 'StartDate' : 'Start',
         sql.VarChar(64),
-        isShamsi ? startDate : startDateMiladi
+        isShamsi ? startDate : startDateMiladi,
       );
       request.input(
         isShamsi ? 'EndDate' : 'End',
         sql.VarChar(64),
-        isShamsi ? endDate : endDateMiladi
+        isShamsi ? endDate : endDateMiladi,
       );
 
       // query to the database and get the records
@@ -132,121 +86,6 @@ exports.FetchData = async (input: {
     });
   });
 };
-
-// exports.FetchTadbirData = (input: { startDate: string; endDate: string }) => {
-//   const { startDate, endDate } = input;
-
-//   // config for your database
-//   const config = {
-//     user: 'TadbirUser',
-//     password: '$$$%%%',
-//     server: '192.168.1.27',
-//     database: 'TEST1400_6_25',
-//     options: {
-//       encrypt: true,
-//       enableArithAbort: true,
-//       cryptoCredentialsDetails: {
-//         minVersion: 'TLSv1',
-//       },
-//     },
-//   };
-
-//   // connect to your database
-//   return new Promise((res, rej) => {
-//     sql.connect(config, (error: Error) => {
-//       if (error) {
-//         console.log(error);
-//         rej(error);
-//       }
-//       // create Request object
-//       const request = new sql.Request();
-//       request.input('StartDate', sql.VarChar(64), startDate);
-//       request.input('EndDate', sql.VarChar(64), endDate);
-
-//       // query to the database and get the records
-//       return request
-//         .execute('___BRBarnameProc___')
-//         .then((result: any, error: any) => {
-//           if (error) {
-//             console.log(error);
-//             rej(error);
-//           }
-
-//           console.log(result, result.recordset.length);
-
-//           res(result.recordset);
-//         })
-//         .catch((error: any) => {
-//           console.log(error);
-//           rej(error);
-//         });
-//     });
-//   });
-// };
-
-// AMORVARIDFAR
-
-// exports.insertPaySamanBankInfo = (input: {
-//   id: number;
-//   agentBranchCode: string;
-//   agentBranchName: string;
-//   balance: number;
-//   branchCode: string;
-//   branchName: string;
-//   date: string;
-//   description: string;
-//   referenceNumber: string;
-//   registrationNumber: string;
-//   serial: number;
-//   serialNumber: string;
-//   transferAmount: number;
-// }) => {
-
-//   // config for your database
-//   const config = {
-//     user: 'TadbirUser',
-//     password: '$$$%%%',
-//     server: '192.168.1.27',
-//     database: 'AMORVARIDFAR',
-//     options: {
-//       encrypt: true,
-//       enableArithAbort: true,
-//       cryptoCredentialsDetails: {
-//         minVersion: 'TLSv1',
-//       },
-//     },
-//   };
-
-//   // connect to your database
-//   return new Promise((res, rej) => {
-//     sql.connect(config, (error: Error) => {
-//       if (error) {
-//         console.log(error);
-//         rej(error);
-//       }
-//       // create Request object
-//       const request = new sql.Request();
-
-//       // query to the database and get the records
-//       return request
-//         .execute('UD__PaySamanBankInfoInsertQuery__')
-//         .then((result: any, error: any) => {
-//           if (error) {
-//             console.log(error);
-//             rej(error);
-//           }
-
-//           console.log(result);
-
-//           res(result.recordset);
-//         })
-//         .catch((error: any) => {
-//           console.log(error);
-//           rej(error);
-//         });
-//     });
-//   });
-// };
 
 exports.insertPaySamanBankInfo = async (input: {
   id: number;
@@ -309,11 +148,7 @@ exports.insertPaySamanBankInfo = async (input: {
       request.input('date', sql.NVarChar(255), date);
       request.input('description', sql.NVarChar(sql.MAX), description);
       request.input('referenceNumber', sql.NVarChar(255), referenceNumber);
-      request.input(
-        'registrationNumber',
-        sql.NVarChar(255),
-        registrationNumber
-      );
+      request.input('registrationNumber', sql.NVarChar(255), registrationNumber);
       request.input('serial', sql.BigInt, serial);
       request.input('serialNumber', sql.NVarChar(255), serialNumber);
       request.input('transferAmount', sql.BigInt, transferAmount);
@@ -346,7 +181,7 @@ exports.insertPaySamanBankInfo = async (input: {
             @registrationNumber,
             @serial,
             @serialNumber,
-            @transferAmount)`
+            @transferAmount)`,
         )
         .then((result: any, error: any) => {
           if (error) {
