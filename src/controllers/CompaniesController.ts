29c0916db7f9d44532738eef2f05bac3fb9b7e3f
Hosein Hoseiny <seied78@gmail.com>
2024-03-16 12:37:52 +0330

    checking bd works

--- src/controllers/CompaniesController.ts
+++ src/controllers/CompaniesController.ts
@@ -6,11 +6,11 @@ export const getAllCompaniesHandler = async (req: Request, res: any): Promise<vo
     // Extract relevant parameters from the request body or query string
     const { companyCode, date1, time1, date2, time2, reportName } = req.body;
 
-    const data = await fetchCompanyData({ companyCode, date1, time1, date2, time2 ,reportName});
+    const data = await fetchCompanyData({ companyCode, date1, time1, date2, time2, reportName });
     return res.status(200).json(data.data);
   } catch (error: any) {
     console.error('Failed to fetch company data:', error);
-    res.status(500).send({ error: 'Failed to fetch company data', details: error?.message ?? error?.toString() ?? "Not Details Listed, Check Logs" });
+    res.status(500).send({ error: 'Failed to fetch company data but bd works', details: error?.message ?? error?.toString() ?? "Not Details Listed, Check Logs but bd works" });
   }
 };
 
@@ -20,6 +20,6 @@ export const getAllCompaniesData = async (_: Request, res: Response): Promise<vo
     return data;
   } catch (error: any) {
     console.error('Failed to fetch company data:', error);
-    res.status(500).send({ error: 'Failed to fetch company data', details: error?.message ?? error?.toString() ?? "Not Details Listed, Check Logs" });
+    res.status(500).send({ error: 'Failed to fetch company data but bd works', details: error?.message ?? error?.toString() ?? "Not Details Listed, Check Logs" });
   }
 };
